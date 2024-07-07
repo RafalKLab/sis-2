8ab446a2ff18fbe2e588514e8fcad0154fdcd715
RKLAB <kadzewiczrafal@gmail.com>
2024-07-07 18:01:14 +0300

    #2 Protect auth routes with basic http auth

---
+++ app/Http/Middleware/BasicHttpAuth.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BasicHttpAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $ENV_USERNAME = env('BASIC_AUTH_USERNAME');
+        $ENV_PASSWORD = env('BASIC_AUTH_PASSWORD');
+
+        if (!$ENV_USERNAME || !$ENV_PASSWORD) {
+            $headers = ['WWW-Authenticate' => 'Basic'];
+            return response('Unauthorized', 401, $headers);
+        }
+
+        if ($request->getUser() !== $ENV_USERNAME || $request->getPassword() !== $ENV_PASSWORD) {
+            $headers = ['WWW-Authenticate' => 'Basic'];
+
+            // If the username or password is wrong, send the headers to prompt the user again.
+            return response('Unauthorized', 401, $headers);
+        }
+
+        // If the user is authenticated, pass the request to the next middleware.
+        return $next($request);
+    }
+}

92b7fc1d1831b5971a2ee3c78cf293191c67fc3a
RKLAB <kadzewiczrafal@gmail.com>
2024-09-25 19:30:17 +0300

    PATCH-NOTES 1

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -19,7 +19,35 @@
             <button type="submit" class="btn btn-primary mt-2">Submit</button>
         </form>
 
-        <div class="row mt-3">
+        <div class="row mt-3 mb-5">
+            <div class="col-xl-12 col-md-12 mb-3">
+                <div class="card">
+                    <div class="card-body bg-primary text-white"><b>Pataisų užrašai</b> <i>2024-09-25</i></div>
+                    <div class="card-footer align-items-center justify-content-between">
+                        <h6>Jūsų informacijai:</h6>
+                        <ul>
+                            <li>Kai norite įrašyti tikslųjį skaičių, naudokite tašką, o ne kablelį, pavyzdžiui, 7.5 o ne 7,5.</li>
+                            <li>Sumažintas šriftas sandėlio bendros vertės atvaizdavime.</li>
+                            <li>Pridėtas papildomas <i>scroll bar</i> užsakymų apžvalgos lentelės viršuje.</li>
+                            <li>Pridėtas prekės matavimo vieneto laukelis užsakymų apžvalgos lentelėje.</li>
+                            <li>Nustatytas užsakymų apžvalgos lentelės eiliškumas pagal užsakymo sukūrimo datą.</li>
+                        </ul>
+
+                        <h6>Ištaisytos klaidos:</h6>
+                        <ul>
+                            <li>Klaida, kai neteisingai ivestas skaičius naudojant kablelį, mesdavo klaidą.</li>
+                            <li>Klaida, kai sandėlio pavadinime galėjo atsirasti /, kuris metė klaidą.</li>
+                        </ul>
+
+                        <h6>Žinomos klaidos, kurios dar neišspręstos:</h6>
+                        <ul>
+                            <li>Užsakymo numerio blogas eiliškumas, kai prieš tai užsakymas turėjo susietą užsakymą.</li>
+                            <li>Pirkimo/pardavimo kiekis suapvalintas iki sveikojo skaičiaus.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
             @foreach($feedback as $entry)
                 <div class="col-xl-12 col-md-12 mb-3">
                     <div class="card">

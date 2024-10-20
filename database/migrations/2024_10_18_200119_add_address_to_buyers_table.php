dc2fe288ce747e909bc74ddc3f190f3a43c60fcf
RKLAB <kadzewiczrafal@gmail.com>
2024-10-20 19:34:14 +0300

    #88 Fix buyers table name

--- database/migrations/2024_10_18_200119_add_address_to_buyers_table.php
+++ database/migrations/2024_10_18_200119_add_address_to_buyers_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('buyers', function (Blueprint $table) {
+        Schema::table('item_buyers', function (Blueprint $table) {
             $table->dropColumn('address');
         });
     }
